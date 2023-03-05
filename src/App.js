f20ad09b1872a010f656e9c8e74292e8bc86b90c
ankitgupta-13 <guptankit0522@gmail.com>
2023-03-05 23:05:46 +0530

    UI updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Movie from "./components/Movie/Movie";
-import movies from "./movie..json";
+import movies from "./movie.json";
 
 function App() {
+  const [searchMoviesArray, setSearchMoviesArray] = useState(movies);
+  const handleSearch = (val) => {
+    console.log(val);
+    val !== ""
+      ? setSearchMoviesArray(
+          movies.filter((movie) => {
+            return movie.Title.toLowerCase().includes(val.toLowerCase());
+          })
+        )
+      : setSearchMoviesArray(movies);
+  };
   return (
     <div className="App">
-      <Header />
+      <Header handleSearch={handleSearch} />
       <div className="main">
-        {movies.map((element,i) => {
-          return (
-            <Movie
-            key={i}
-              title={element.Title}
-              year={element.Year}
-              img={element.Poster}
-            />
-          );
-        })}
+        {searchMoviesArray.length > 0 ? (
+          searchMoviesArray.map((element, i) => {
+            return (
+              <Movie
+                key={i}
+                title={element.Title}
+                year={element.Year}
+                img={element.Poster}
+              />
+            );
+          })
+        ) : (
+          <h1>No movie found!</h1>
+        )}
       </div>
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
-import "./Header.css"
-const Header=()=>{
-    return(
-        <div>
-            <h1>Marvel DC Movies</h1>
-        </div>
-    )
-}
-export default Header
+import { useState } from "react";
+import "./Header.css";
+const Header = ({handleSearch}) => {
+    const [search,setSearch]=useState("");
+  return (
+    <div className="container">
+      <img
+        src="https://cdn.pixabay.com/photo/2016/12/28/08/15/hummingbird-1935665_960_720.png"
+        alt="Logo not found!"
+      />
+      <h1>Marvel DC Movies</h1>
+      <input value={search} onChange={(e)=>{setSearch(e.target.value); handleSearch(e.target.value)}} type="text" placeholder="Search a movie..." className="searchBar" />
+    </div>
+  );
+};
+export default Header;
